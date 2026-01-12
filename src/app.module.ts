e7fc01c546e8ddc0dfa5151b5740ab90dfc3b4f8
JuanrCama07 <juanrcama07@gmail.com>
2026-01-12 14:23:58 -0500

    Inicialización de Proyecto Bootstrap NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
+import { TasksModule } from './tasks/tasks.module';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    RolesModule,
+    TasksModule,
+    AssignmentsModule,
+    CommonModule,
+    ConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
